dc03e6dba64aa6ada79e1e2720ca4c17ae9b4b49
adityasinghskit@gmail.com <adityasinghskit@gmail.com>
2024-08-04 13:27:01 +0530

    added chat component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import "./App.css";
 import { User, MessageCircle, X, Heart } from "lucide-react";
 
+const App = () => {
+const [currentScreen, setCurrentScreen] = useState("profile");
+const [input, setInput] = useState('');
 const imagePrefix = "http://localhost:9000/images/";
 
 const ProfileSelector = () => {
@@ -73,16 +77,58 @@ const MatchesList = () => {
   );
 };
 
-const App = () => {
+const ChatScreen = () => {
+  
+  return (
+    <div className="rounded-lg shadow-lg p-4">
+      <h2 className="text-2xl font-bold mb-4">Chats</h2>
+      <div className="h-[50vh] border rounded overflow-y-auto mb-4 p-4">
+        {["Hi", "How are you?", "I am gooooddd."].map((message, index) => (
+          <div key={index}>
+            <div className="mb-4 p-2 rounded bg-gray-100">{message}</div>
+          </div>
+        ))}
+      </div>
+      <div className="flex">
+        <input 
+        type="text" 
+        value={input} 
+        onChange={(event) => setInput(event.target.value)}
+        className="border flex-1 rounded p-2 mr-2"/>
+        <button 
+        className="bg-blue-500 text-white rounded p-2"
+        onClick={() => handleSend()}>Send</button>
+      </div>
+    </div>
+  );
+};
+
+const handleSend = () => {
+  if(input.trim()){
+    console.log("handle send");
+  }
+}
+  
+  const renderScreen = () => {
+    switch (currentScreen) {
+      case "profile":
+        return <ProfileSelector />;
+      case "matches":
+        return <MatchesList />;
+      case "chat":
+        return <ChatScreen/>;
+    }
+  };
+
+
   return (
     <>
       <div className="max-w-md mx-auto p-4">
         <nav className="flex justify-between mb-4">
-          <User />
-          <MessageCircle />
+          <User onClick={() => setCurrentScreen("profile")} />
+          <MessageCircle onClick={() => setCurrentScreen("matches")} />
         </nav>
-        {/* <ProfileSelector /> */}
-        <MatchesList />
+        {renderScreen()}
       </div>
     </>
   );
